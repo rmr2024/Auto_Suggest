26ca149b87dca585b2d4621f4a3deaaebb574a50
rmr2024 <meenu2024@gmail.com>
2026-07-04 10:08:54 +0530

    Capitalised gender

--- frontend/index.js
+++ frontend/index.js
@@ -40,7 +40,8 @@ function getRandomUser()
       let imageUrl = parsedData.results[0].picture.large;
 
       // gender
-      document.getElementById("card-gender").innerText=gender;
+      document.getElementById('card-gender').innerText = gender.charAt(0).toUpperCase() + gender.slice(1).toLowerCase();
+
       // name
       document.getElementById("card-name").innerText=name
       // img
