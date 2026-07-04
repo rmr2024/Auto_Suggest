f26f52f61978b695206099c8dcd71fb4dbb28c18
rmr2024 <meenu2024@gmail.com>
2026-07-04 09:40:58 +0530

    made changes to css and implemented random user function

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,36 @@
-console.log("Hello from JS");
-alert("Hello from JS");
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "./images/jane.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "./images/john.png"
+  }
+];
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+  //gender
+  document.getElementById("card-gender").innerText=users[curUserId].gender;
+}
+
+function getRandomUser(){
+  const randomIndex = Math.floor(Math.random() * users.length);
+  curUserId = randomIndex;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+  //gender
+  document.getElementById("card-gender").innerText=users[curUserId].gender;
+}
